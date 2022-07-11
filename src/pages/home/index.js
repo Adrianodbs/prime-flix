dc9bd8aac7a85341e6a27eea508414caa8b2ac43
Adriano <adriano_alves_2008@hotmail.com>
2022-07-11 13:42:47 -0300

    listando filmes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from 'react'
 import api from '../../services/api'
+import { Link } from 'react-router-dom'
+import './home.css'
 
 export default function Home() {
   const [filmes, setFilmes] = useState([])
@@ -16,10 +18,28 @@ export default function Home() {
         }
       })
 
-      console.log(response.data.results)
+      // console.log(response.data.results)
+      setFilmes(response.data.results.slice(0, 10))
     }
 
     loadFilmes()
   }, [])
-  return <h1>Bem vindo a Home</h1>
+  return (
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map(filme => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
