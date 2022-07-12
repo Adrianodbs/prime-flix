2701a1b71be4f2e81988f1018725dd9846f4d545
Adriano <adriano_alves_2008@hotmail.com>
2022-07-12 15:22:52 -0300

    Manipulando lista de filmes

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,9 +10,25 @@ export default function Favoritos() {
     // O dado vem como string, então precisamos converter para um array
     setFilmes(JSON.parse(minhaLista) || [])
   }, [])
+
+  function excluirFilme(id) {
+    // Primeiro vamos filtar todos os filmes e retirar aquele que eu cliquei
+    let filtroFilmes = filmes.filter(item => {
+      // Ou seja, vou devolver todos os filmes, menos o que eu cliquei
+      return item.id !== id
+    })
+
+    setFilmes(filtroFilmes)
+    // Depois eu preciso salvar no localStorage
+    localStorage.setItem('@primeflix', JSON.stringify(filtroFilmes))
+  }
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+      {filmes.length === 0 && (
+        <span>Você não possui nenhum filme salvo :(</span>
+      )}
 
       <ul>
         {filmes.map(item => {
@@ -21,7 +37,7 @@ export default function Favoritos() {
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           )
