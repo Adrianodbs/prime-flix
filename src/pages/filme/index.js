4ada1123e953be30761d4c2d15e1f0bde94724b0
Adriano <adriano_alves_2008@hotmail.com>
2022-07-12 11:08:47 -0300

    detalhes do filme

--- src/pages/filme/index.js
+++ src/pages/filme/index.js
@@ -1,3 +1,64 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import api from '../../services/api'
+
+import './filme-info.css'
+
 export default function Filme() {
-  return <h1>Bem vindo aos filmes</h1>
+  const { id } = useParams()
+  const [filme, setFilme] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: '4d1403d051e04485d72f451e01622538',
+            language: 'pt-BR'
+          }
+        })
+        .then(response => {
+          setFilme(response.data)
+          setLoading(false)
+        })
+        // Em caso de erro ele vai cair no catch
+        .catch(() => {
+          console.log('filme não encontrado')
+        })
+    }
+
+    loadFilme()
+
+    return () => {}
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes ...</h1>
+      </div>
+    )
+  }
+  return (
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
+    </div>
+  )
 }
